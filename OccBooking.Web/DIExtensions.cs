76d64d5fde4f339af3b695f29bbb164482ac4e28
Michal Miciak <maq0297@gmail.com>
2019-12-09 17:59:02 +0100

    Backend improvements

--- OccBooking.Web/DIExtensions.cs
+++ OccBooking.Web/DIExtensions.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Autofac;
+using AutoMapper.Configuration;
+using OccBooking.Application.EventHandlers;
 using OccBooking.Application.Handlers;
+using OccBooking.Application.Services;
+using OccBooking.Common.Dispatchers;
 using OccBooking.Common.Hanlders;
+using OccBooking.Common.Infrastructure;
 using OccBooking.Common.Types;
+using OccBooking.Persistance.Repositories;
 
 namespace OccBooking.Web
 {
@@ -53,9 +59,45 @@ namespace OccBooking.Web
             }
         }
 
-        public static bool IsAssignableToGenericInterface(this Type givenType, Type genericType)
+        private static bool IsAssignableToGenericInterface(this Type givenType, Type genericType)
         {
             return givenType.GetInterfaces().Any(x => x.IsGenericType && x.GetGenericTypeDefinition() == genericType);
+        }
+
+        public static void RegisterEventHandlers(this ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.GetAssembly(typeof(ReservationRequestRejectedEventHandler)))
+                .AsClosedTypesOf(typeof(IEventHandler<>));
+        }
+
+        public static void RegisterServices(this ContainerBuilder builder)
+        {
+            builder.RegisterType<EmailService>().As<IEmailService>();
+        }
+
+        public static void RegisterDispatchers(this ContainerBuilder builder)
+        {
+            builder.RegisterType<CqrsDispatcher>().As<ICqrsDispatcher>();
+            builder.RegisterType<EventDispatcher>().As<IEventDispatcher>();
+        }
+
+        public static void RegisterRepositories(this ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.GetAssembly(typeof(IRepository<>)))
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces();
+        }
+
+        public static void RegisterAppSettings(this ContainerBuilder builder,
+            Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            var emailConfigurationSectionSection = configuration.GetSection("Email");
+            var appSettings = new AppSettings(emailConfigurationSectionSection["EmailAddress"],
+                emailConfigurationSectionSection["EmailPassword"],
+                emailConfigurationSectionSection["EmailName"],
+                emailConfigurationSectionSection["SmtpHost"],
+                int.Parse(emailConfigurationSectionSection["SmtpPort"]));
+            builder.Register(c => appSettings).SingleInstance();
         }
     }
 }
