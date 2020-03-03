0057e470491e95f47e564deda2ba5f23def8d07e
Michal Miciak <maq0297@gmail.com>
2020-03-03 18:33:48 +0100

    refactor

--- OccBooking.Persistence.Tests/Repositories/ReservationRequestRepositoryTests.cs
+++ OccBooking.Persistence.Tests/Repositories/ReservationRequestRepositoryTests.cs
@@ -21,9 +21,8 @@ namespace OccBooking.Persistence.Tests.Repositories
     {
         public static Client CorrectClient => new Client("Michal", "Kowalski", "michal@michal.com", "505111111");
         public static Address CorrectAddress => new Address("Akacjowa", "Orzesze", "43-100", "śląskie");
-
         public static Place CorrectPlace => new Place(new Guid("619e8c4e-69ae-482a-98eb-492afe60352b"), "Calvados",
-            false, "", CorrectAddress, new Guid("4ea10f9e - ae5f - 43a1 - acfa - c82b678e6ee1"));
+            false, "", CorrectAddress, new Guid("4ea10f9e-ae5f-43a1-acfa-c82b678e6ee1"));
 
         [Fact]
         public async Task GetImpossibleReservationRequestsAsync_ShouldWork()
