851ada3c273658f361b14100a1cc3217bf511220
Michal Miciak <maq0297@gmail.com>
2020-03-14 11:50:44 +0100

    migrations for removing navigation properties between hall and place

---
+++ OccBooking.Persistence/Migrations/20200314100546_RemovingNavigationPropertiesBetweenPlaceAndHall.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OccBooking.Persistence.Migrations
+{
+    public partial class RemovingNavigationPropertiesBetweenPlaceAndHall : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Halls_Places_PlaceId",
+                table: "Halls");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PlaceId",
+                table: "Halls",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Halls_Places_PlaceId",
+                table: "Halls",
+                column: "PlaceId",
+                principalTable: "Places",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Halls_Places_PlaceId",
+                table: "Halls");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PlaceId",
+                table: "Halls",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Halls_Places_PlaceId",
+                table: "Halls",
+                column: "PlaceId",
+                principalTable: "Places",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
